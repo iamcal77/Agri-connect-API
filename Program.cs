2d9136b8be56b9237faaee39c7fcf28701547a79
Calvin Kipkirui <calvinkipkirui99@gmail.com>
2025-07-30 19:24:48 +0300

    firt commit

--- Program.cs
+++ Program.cs
@@ -78,7 +78,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
             "http://localhost:3000",
-            "https://agriconnect2.netlify.app"
+            "https://agriconnect2.netlify.app",
+            "https://task-fronend.onrender.com"
         )
         .AllowAnyMethod()
         .AllowAnyHeader();
